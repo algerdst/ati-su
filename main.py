580e2d0c04ed5afb234058860cf9538425d26e0b
Альгерд <stadolnikofffgmail.com>
2024-03-03 12:36:11 +0300

    update re

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,8 +8,21 @@ import re
 
 import time
 
+# bet = int(input('Введите разницу между ставками (просто число, без %)'))
+bet = 10
+# from_ = input('Введите пункт отправки')
+from_ = "вологда"
+# to_ = input('Введите пункт доставки')
+to_ = "москва"
+# weight = input('Введите вес (просто число, без т.)')
+weight = 10
+
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
+
+with open(f'Результат_{from_}_{to_}_ставка={bet}_вес={weight}.csv', 'w', newline='', encoding='utf-8-sig') as file:
+    writer = csv.writer(file, delimiter=';')
+    writer.writerow(['Ставка с НДС', 'Ставка без НДС', 'Заданная разница %', 'Разница между ставками в %', 'Контакты'])
 
 
 def login_func(browser):
@@ -40,17 +55,17 @@ def fing_cargos(browser):
                                                                                                    'button')
     from_input.click()
     from_input.clear()
-    from_input.send_keys('самара')
+    from_input.send_keys(from_)
     time.sleep(1)
     browser.find_element(By.CLASS_NAME, 'Geo_suggestion__FxHag').click()
     where_input.click()
     where_input.clear()
-    where_input.send_keys('москва')
+    where_input.send_keys(to_)
     time.sleep(1)
     browser.find_element(By.CLASS_NAME, 'Geo_suggestion__FxHag').click()
     weight_input.click()
     weight_input.clear()
-    weight_input.send_keys(10)
+    weight_input.send_keys(weight)
     find_cargo.click()
     time.sleep(5)
 
@@ -74,17 +89,54 @@ with webdriver.Chrome(options) as browser:
                 block.find_element(By.CLASS_NAME, 'glz-button').click()
             except:
                 pass
-            items = [
-                m.group()
-                for m in re.finditer(r"((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}", block.text)
-            ]
-            number=''
-            for i in items:
-                if i[0] == '+':
-                    number+=i
-                    number+=' '
-            print(number)
-            count+=1
+
+            if 'с НДС' in block.text and 'без НДС' in block.text:
+                before_sNDS = block.text.split(' рубс НДС')[0][::-1]
+                before_bezNDS = block.text.split(' руббез НДС')[0][::-1]
+            else:
+                continue
+            sNDS = ''
+            bezNDS = ''
+            for i in before_sNDS:
+                if i.isdigit() or i == ' ':
+                    sNDS += i
+                else:
+                    break
+            for i in before_bezNDS:
+                if i.isdigit() or i == ' ':
+                    bezNDS += i
+                else:
+                    break
+            try:
+                sNDS = int(sNDS[::-1].replace(' ', ''))
+                bezNDS = int(bezNDS[::-1].replace(' ', ''))
+            except:
+                continue
+            difference = (sNDS - bezNDS) / (bezNDS / 100)
+            difference = round(difference, 2)
+            if difference > bet:
+                cargo = True
+            else:
+                cargo = False
+            if cargo == False:
+                continue
+            else:
+                items = [
+                    m for m in re.findall(r'\+\(?\d+(?:[-()]*\d+[-()]*)+', block.text)
+                ]
+                number = ''
+                for i in items:
+                    if i[0] == '+':
+                        number += i
+                        number += '\n'
+                number='"'+number+'"'
+                difference = str(difference).replace('.', ',') + '%'
+                str_bet = str(bet) + '%'
+                with open(f'Результат_{from_}_{to_}_ставка={bet}_вес={weight}.csv', 'a', newline='', encoding='utf-8-sig') as file:
+                    writer = csv.writer(file, delimiter=';')
+                    writer.writerow(
+                        [sNDS, bezNDS, str_bet, difference, number])
+                count += 1
         next_page.click()
         time.sleep(2)
-print(count)
+print('Найдено подходящих записей', count)
